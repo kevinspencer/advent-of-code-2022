777d2f0127c6861feb423bf75aed3d85fefcd8e6
kevinspencer <kevin@kevinspencer.org>
2022-12-06 15:27:36 -0700

    now outputting both answers

--- day-six/daysix-packet-marker.pl
+++ day-six/daysix-packet-marker.pl
@@ -3,21 +3,27 @@
 use strict;
 use warnings;
 
+my $marker_position = shift;
+
+die "Need a marker position!\n" if ((! $marker_position) || ($marker_position !~ /\d+/));
+
 open(my $fh, '<', 'daysix-input.txt') || die "Could not open daysix-input.txt - $!\n";
 my $string = <$fh>;
 close($fh);
 
-my @last_four_chars;
+my @tracking_chars;
 my $char_position = 0;
 for my $char (split//, $string) {
     $char_position++;
-    push(@last_four_chars, $char);
-    if (@last_four_chars == 4) {
-        my $unique = { map { $_ => 1 } @last_four_chars };
-        if ($unique && (keys %$unique == 4)) {
+    push(@tracking_chars, $char);
+    if (@tracking_chars == $marker_position) {
+        # convert the array element values into hash keys and uniq them
+        my $unique = { map { $_ => 1 } @tracking_chars };
+        # if our unique count doesn't match our $marker_position, keep going
+        if ($unique && (keys %$unique == $marker_position)) {
             print "Found unique at marker: $char_position\n";
-            exit();
+            last;
         }
-        shift(@last_four_chars);
+        shift(@tracking_chars);
     }
 }
